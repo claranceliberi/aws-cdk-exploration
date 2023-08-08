c0c5f248d8ba3e8bec2e7410975e549f3294bd92
Clarance Liberiste Ntwari <60586899+claranceliberi@users.noreply.github.com>
2023-08-08 23:02:44 +0200

    ignore useless files

---
+++ space-finder/src/infra/Launcher.ts
@@ -0,0 +1,14 @@
+
+import { App } from 'aws-cdk-lib';
+import { DataStack } from './stacks/DataStack';
+import { LambdaStack } from './stacks/LambdaStack';
+import { ApiStack } from './stacks/ApiStack';
+
+const app = new App();
+
+new DataStack(app, 'DataStack');
+const lambdaStack = new LambdaStack(app, 'LambdaStack');
+
+new ApiStack(app, 'ApiStack', {
+    spacesLambdaIntegration: lambdaStack.spacesLambdaIntegration
+})
